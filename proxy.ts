1c847d8bce08891376fa19dabbedddef9081702f
Adınız <email@adresiniz.com>
2026-04-03 14:45:42 +0300

    fix middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req) {
+export function proxy(req: Request) {
+  const url = new URL(req.url);
   const host = req.headers.get("host");
 
   if (host === "camerapick.sedatpala.site") {
-    return NextResponse.rewrite(new URL("/camerapick", req.url));
+    url.pathname = "/camerapick";
+    return NextResponse.rewrite(url);
   }
 
   return NextResponse.next();
