407400dd12b0078d738e10d1ad432eeef6ae1e5d
Adınız <email@adresiniz.com>
2026-04-03 14:38:24 +0300

    first version

---
+++ app/camerapick/page.tsx
@@ -0,0 +1,25 @@
+    export default function CameraPickPage() {
+return (
+  <main className="min-h-screen bg-white text-black p-10">
+    <h1 className="text-4xl font-bold">CameraPick</h1>
+
+    <p className="mt-4 text-lg">
+      Compare cameras easily and make better decisions.
+    </p>
+
+    <a
+      className="mt-6 inline-block underline"
+      href="https://apps.apple.com/tr/app/camerapick/id6760162723"
+      target="_blank"
+    >
+      Download on the App Store
+    </a>
+  </main>
+);
+}
+//  page.tsx
+//  
+//
+//  Created by Sedat Pala on 3.04.2026.
+//
+
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white text-black p-10">
+      <h1 className="text-4xl font-bold">Sedat Pala</h1>
+      <p className="mt-4 text-lg">Doctor • App Builder</p>
+
+      <div className="mt-10">
+        <h2 className="text-2xl font-semibold">My App</h2>
+
+        <a
+          className="mt-4 inline-block underline"
+          href="https://camerapick.sedatpala.site"
+        >
+          CameraPick
+        </a>
+      </div>
+    </main>
+  );
+}
